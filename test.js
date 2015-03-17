54e463c191b3f13c90428db86bc6653b4eb0a786
keen <ehrudxo@hotmail.com>
2015-03-17 09:44:58 +0900

    daily,weekly wow

--- test.js
+++ test.js
@@ -1,39 +1,4 @@
-var request = require("request");
-var env = require('jsdom').env;
-var fs = require('fs');
 var moment = require('moment');
-
-var sum = [];
-request('https://github.com/trending?since=weekly', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    env(body, function (errors, window) {
-      if(errors)console.log(errors);
-      var $ = require('jquery')(window);
-      $(".repo-list-item").each(function(index, valueObj){
-        var url = $(valueObj).find(".repo-list-name>a").attr("href");
-        var description = $(valueObj).find(".repo-list-description").text().trim();
-        var meta = $(valueObj).find(".repo-list-meta").text().trim().replace(/\n/g,"").split("•");
-        var language,starnum;
-        if( meta.length==3 ){
-          language = meta[0].trim();
-          starnum = parseInt(meta[1].substring(0,meta[1].indexOf("stars") ).replace(/,/g,"") );
-        }else if(meta.length==2){
-          language = "none"
-          starnum = parseInt(meta[0].substring(0,meta[0].indexOf("stars") ).replace(/,/g,"") );
-        }else{
-          console.log("exception occurred!");
-          throw "Exception";
-        }
-        sum.push({url:url,description:description, language: language, starnum: starnum});
-        if( index == 24){
-          fs.writeFile(__dirname + "/weekly/", JSON.stringify(sum) , function(err) {
-            if(err) {
-                return console.log(err);
-            }
-            console.log("The file was saved!");
-        });
-        }
-      });
-    });
-  }
-})
+var gh = require('./github-trend');
+console.log(moment().format("YYYY-MM-DD").toString());
+console.log(gh.parseToJson("daily"));
