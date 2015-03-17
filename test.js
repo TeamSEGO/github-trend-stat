003324945038cecb260b124ff524abe825cc559c
keen <ehrudxo@hotmail.com>
2015-03-17 09:45:57 +0900

    weekly saved

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var moment = require('moment');
 var gh = require('./github-trend');
 console.log(moment().format("YYYY-MM-DD").toString());
-console.log(gh.parseToJson("daily"));
+console.log(gh.parseToJson("weekly"));
