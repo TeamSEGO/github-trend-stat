a2389cbf959ab9d0fc28c6cd4646400ea661f3f6
keen <ehrudxo@hotmail.com>
2015-03-20 09:50:47 +0900

    .

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,10 @@
+var jsons = ["2015-03-17","2015-03-18","2015-03-19","2015-03-20"];
 $( document ).ready(function(){
   console.log('This would be the main JS file.');
+  Async.eachSeries(jsons, function(path){
+    $.ajax({url: "./javascript/"+path,
+  }).done(function(data){
+    console.log(data);
+  });
+  })
 });
