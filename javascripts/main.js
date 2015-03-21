00f48f1254f7d85efe991db2f393e991e11273f2
keen <ehrudxo@hotmail.com>
2015-03-21 17:11:57 +0900

    add daily function

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,8 @@
-var jsons = ["2015-03-17","2015-03-18","2015-03-19","2015-03-20"];
+var jsons = ["2015-03-17","2015-03-18","2015-03-19","2015-03-20","2015-03-20"];
 $( document ).ready(function(){
   console.log('This would be the main JS file.');
-  Async.eachSeries(jsons, function(path){
-    $.ajax({url: "./javascript/"+path,
+  async.eachSeries(jsons, function(path){
+    $.ajax({url: "./daily/"+path+".json",
   }).done(function(data){
     console.log(data);
   });
