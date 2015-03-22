e9b71b0e36e031b5582f811299cdbe51cce718e3
keen <ehrudxo@hotmail.com>
2015-03-22 22:45:19 +0900

    main js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,23 @@
-var jsons = ["2015-03-17","2015-03-18","2015-03-19","2015-03-20","2015-03-20"];
+var korDateFmt ="YYYY-MM-DD";
+var startMoment = moment("2015-03-17");
+var getDateSeries = function( from, to ){
+  var diff_y_1 = to.diff(from,'days') - 1;
+  var days = [];
+  days.push(from.format(korDateFmt).toString());
+  while(true){
+    days.push(from.add(1,'days').format(korDateFmt).toString());
+    if(!diff_y_1--)break;
+  }
+  return days;
+}
+
+
 $( document ).ready(function(){
-  console.log('This would be the main JS file.');
-  async.eachSeries(jsons, function(path){
-    $.ajax({url: "./daily/"+path+".json",
-  }).done(function(data){
-    console.log(data);
-  });
+  var days = getDateSeries(startMoment,moment());
+  console.log(days);
+  async.each( days, function(path){
+    $.ajax({url: "./daily/"+path+".json"}).done(function(data){
+      console.log(path, data);
+    });
   })
 });
