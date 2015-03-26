7baf98575513e2a7843e43ee45992b1a11c41143
kt.do <kt.do@samsung.com>
2015-03-26 16:54:20 +0900

    it has bug. but for checking mobile browser

--- javascripts/main.js
+++ javascripts/main.js
@@ -22,43 +22,49 @@ $( document ).ready(function(){
     $.ajax({url: "./daily/"+day+".json"}).done(function(data){
       var idx = getIndexFromNums(days,day);
       var len = days.length;
-      stackData( idx, len, day, data, cb );
-    });
+      console.log(idx, days);
+      stackData( idx, len, day, JSON.parse(data), cb );
+    }).fail(function(){cb();});
   }, graph );
 });
 var stackData = function( idx, lenOfDay, day, data, cb ){
+  var getArrayFromData = function(lenOfDay){
+    var arr = [];
+    arr.push(0);
+    while(lenOfDay--)arr.push(0);
+    return arr;
+  }
   async.each(data, function(module, ccb){
     var url = module['url'];
     var day = data;
     var starnum = module['starnum'];
-    grpData["datasets"];
 
     var len = grpData["datasets"].length;
     var index = -1;
+
     for(var i=0;i<len;i++){
-      if(grpData["datasets"][i]["label"]) index=i;
+      if(grpData["datasets"][i]["label"]==url) index=i;
     }
     if(index > 0){
       grpData["datasets"][index]["data"][idx] = starnum;
     }else{
       var ds = {};
-      ds["label"] =  url;
+      ds["label"] = url;
       var randomColor = function(){
         var r = Math.floor((Math.random() * 255) + 1);
         var g = Math.floor((Math.random() * 255) + 1);
         var b = Math.floor((Math.random() * 255) + 1);
         return r +"," +g+"," + b;
       }();
-      console.log(randomColor,idx, starnum);
       ds["fillColor"] = "rgba("+randomColor+",0.2)";
       ds["strokeColor"] = "rgba("+randomColor+",1)";
       ds["pointColor"] = "rgba("+randomColor+",1)";
       ds["pointStrokeColor"] = "#fff";
       ds["pointHighlightFill"]= "#fff";
       ds["pointHighlightStroke"] = "rgba("+randomColor+",1)";
-      ds["data"]=[];
+      ds["data"] = getArrayFromData(lenOfDay);
       ds["data"][idx] = starnum;
-      console.log(ds);
+      console.log(ds["label"]);
       grpData["datasets"].push(ds);
     }
     ccb();
@@ -72,34 +78,38 @@ var getIndexFromNums = function (days,day){
     if(days[i]==day) return i;
   }
 }
-/*
-var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
+var option = {
+    scaleShowGridLines : true,
+    scaleGridLineColor : "rgba(0,0,0,.05)",
+    scaleGridLineWidth : 1,
+    scaleShowHorizontalLines: true,
+    scaleShowVerticalLines: true,
+    bezierCurve : true,
+    bezierCurveTension : 0.4,
+    pointDot : true,
+    pointDotRadius : 4,
+    pointDotStrokeWidth : 1,
+    pointHitDetectionRadius : 20,
+    datasetStroke : true,
+    datasetStrokeWidth : 2,
+    datasetFill : true,
+    legendTemplate : '<ul>'
+                  +'<% for (var i=0; i<datasets.length; i++) { %>'
+                    +'<li>'
+                    +'<span style=\"background-color:<%=datasets[i].strokeColor%>\">&nbsp;&nbsp;&nbsp;</span>'
+                    +'<% if (datasets[i].label) { %><a href=\"http://www.github.com/<%= datasets[i].label %>\"><%= datasets[i].label %></a><% } %>'
+                  +'</li>'
+                +'<% } %>'
+              +'</ul>',
+      customTooltips: function(tooltip) {
+                        if (!tooltip) {
+                            return;
+                        }
+                  }
 };
-*/
 var graph = function(){
   var ctx = document.getElementById("trend-stat").getContext("2d");
-  var myLineChart = new Chart(ctx).Line(grpData);
+  var statLineChart = new Chart(ctx).Line(grpData,option);
+  var legend = statLineChart.generateLegend();
+  $('#trend-legend').append(legend);
 }
