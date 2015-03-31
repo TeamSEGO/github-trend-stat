82f6a5831f64b274114912fa6ce5d33757f50b15
keen <ehrudxo@hotmail.com>
2015-03-31 09:39:06 +0900

    some test code added for lowdb

--- test.js
+++ test.js
@@ -5,4 +5,8 @@ var today = moment();
 var aMonthAgo = moment().subtract(1,'months');
 console.log(today.format("YYYY-MM-DD").toString());
 console.log(aMonthAgo.format("YYYY-MM-DD").toString());
-db('posts').push({ title: 'lowdb is awesome'})
+//db('posts').push({ "2015-03-31": "awesome?"});
+var dd= db('posts').findIndex( function(chr){
+  return chr["2015-03-31"];
+});
+console.log(dd);
